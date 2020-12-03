a545bfd886192a4a3ce9d95c2da4e6e0ce436e14
derese getachew <deresegetachew@gmail.com>
2020-12-03 10:00:22 +0300

    implemented Money add subtract and validations for our value object

--- DDDPractice.Domain/Money.cs
+++ DDDPractice.Domain/Money.cs
@@ -5,12 +5,20 @@ namespace DDDPractice.Domain
     public class Money : ValueObject<Money>
     {
         
-        public int TenCentBalance { get; private set; }
-        public int QuarterCentBalance { get; private set; }
-        public int FiftyCentBalance { get; private set; }
-        public int OneBirrBalance { get;  private set; }
-        public int FiveBirrBalance { get; private set; }
-        public int TenBirrBalance { get; private set; }
+        public int TenCentBalance { get;}
+        public int QuarterCentBalance { get; }
+        public int FiftyCentBalance { get;  }
+        public int OneBirrBalance { get;   }
+        public int FiveBirrBalance { get; }
+        public int TenBirrBalance { get;  }
+
+        public decimal Amount => TenCentBalance * 0.1m +
+                                 QuarterCentBalance * 0.25m +
+                                 FiftyCentBalance * 0.5m +
+                                 OneBirrBalance +
+                                 FiveBirrBalance * 5 +
+                                 TenBirrBalance * 10;
+
 
         public Money(int tenCentCount, 
                 int quarterCentCount, int fiftyCentCount, 
@@ -51,6 +59,19 @@ namespace DDDPractice.Domain
                     m1.TenBirrBalance + m2.TenBirrBalance
                 );
             return sum;
+        }
+
+        public static Money operator -(Money m1, Money m2)
+        {
+            Money diff = new Money(
+                m1.TenCentBalance - m2.TenCentBalance,
+                m1.QuarterCentBalance - m2.QuarterCentBalance,
+                m1.FiftyCentBalance - m2.FiftyCentBalance,
+                m1.OneBirrBalance - m2.OneBirrBalance,
+                m1.FiveBirrBalance - m2.FiveBirrBalance,
+                m1.TenBirrBalance - m2.TenBirrBalance);
+
+            return diff;
         }
 
         protected override bool EqualsCore(Money other)
--- DDDPractice.Tests/MoneySpecs.cs
+++ DDDPractice.Tests/MoneySpecs.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using DDDPractice.Domain;
 using FluentAssertions;
+using FluentAssertions.Common;
 using Xunit;
 
 namespace DDDPractice.Tests
@@ -70,5 +71,68 @@ namespace DDDPractice.Tests
 
             action.Should().Throw<InvalidOperationException>();
         }
+        
+        
+        [Theory]
+        [InlineData(0,0,0,0,0,0,0)]
+        [InlineData(1,0,0,0,0,0,0.1)]
+        [InlineData(1,2,0,0,0,0,0.6)]
+        [InlineData(1,2,3,0,0,0,2.1)]
+        [InlineData(1,2,3,4,0,0,6.1)]
+        [InlineData(1,2,3,4,5,0,31.1)]
+        [InlineData(1,2,3,4,5,6,91.1)]
+        public void amount_is_calculated_correctly(
+            int tenCentBalance,
+            int quarterCentBalance,
+            int fifityCentBalance,
+            int oneBirrBalance,
+            int fiveBirrBalance,
+            int tenBirrBalance,
+            double expectedAmount)
+        {
+            Money money = new Money(
+                tenCentBalance,
+                quarterCentBalance,
+                fifityCentBalance,
+                oneBirrBalance,
+                fiveBirrBalance,
+                tenBirrBalance);
+
+            
+            decimal d =  Convert.ToDecimal(expectedAmount);
+            
+            money.Amount.Should().Be(d);
+        }
+
+        [Fact]
+        public void Subtraction_of_two_money_objects_produces_correct_result()
+        {
+            Money m1 = new Money(10,10,10,10,10,10);
+            Money m2 = new Money(1, 2, 3, 4, 5, 6);
+
+            Money result = m1 - m2;
+
+            result.TenCentBalance.Should().Be(9);
+            result.QuarterCentBalance.Should().Be(8);
+            result.FiftyCentBalance.Should().Be(7);
+            result.OneBirrBalance.Should().Be(6);
+            result.FiveBirrBalance.Should().Be(5);
+            result.TenBirrBalance.Should().Be(4); 
+        }
+
+        [Fact]
+        public void Cannot_subtract_more_than_balance()
+        {
+            Money m1 = new Money(0,1,0,0,0,0);
+            Money m2 = new Money(1,0,0,0,0,0);
+
+            Action action = () =>
+            {
+                Money money = m1 - m2;
+            };
+
+            action.Should().Throw<InvalidOperationException>();
+        }
+        
     }
 }
